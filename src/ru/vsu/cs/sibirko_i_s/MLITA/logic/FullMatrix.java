b8fb793c7c95f3554a12b6d1baf41939aa7b511b
darkrai36 <sismain36@gmail.com>
2024-04-14 13:13:34 +0300

    Delete class 'SquareMatrix' cuz I can't fast work with it.

--- src/ru/vsu/cs/sibirko_i_s/MLITA/logic/FullMatrix.java
+++ src/ru/vsu/cs/sibirko_i_s/MLITA/logic/FullMatrix.java
@@ -3,43 +3,24 @@ package ru.vsu.cs.sibirko_i_s.MLITA.logic;
 import java.util.Random;
 
 public class FullMatrix {
-    private int height;
-    private int weight;
-    public int[][] matrix;
-
-    public FullMatrix(int height, int weight) {
-        if (height > 0 && weight > 0) {
-            this.height = height;
-            this.weight = weight;
-            matrix = new int[height][weight];
-        } else {
-            throw new NullPointerException("Matrix is empty, try again");
-        }
-    }
-
-    public int[][] buildSquareMatrix(FullMatrix object) {
-        if (height <= 0 || weight <= 0 || height < weight - 1) {
+    public static int[][] buildSquareMatrix(int[][] matrix) {
+        int height = matrix.length;
+        int weight = matrix[0].length;
+        if (height == 0 || weight == 0) {
             throw new NullPointerException("Invalid matrix's parameters");
+        } else if (height != weight - 1) {
+            throw new NullPointerException("Row's count must be = (col's count - 1)");
         } else {
             int[][] squareMatrix = new int[height][weight - 1];
-            for (int i = 0; i < object.matrix.length; i++) {
-                for (int j = 0; j < object.matrix[0].length - 1; j++) {
-                    squareMatrix[i][j] = object.matrix[i][j];
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < weight - 1; j++) {
+                    squareMatrix[i][j] = matrix[i][j];
                 }
             }
             return squareMatrix;
         }
     }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void randomInput() {
+    public static void randomInput(int[][] matrix) {
         Random rnd = new Random();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -47,11 +28,11 @@ public class FullMatrix {
             }
         }
     }
-    public void printMatrix() {
+    public static void printMatrix(int[][] matrix) {
         System.out.println("Matrix: ");
-        for (int[] rows : matrix) {
-            for (int value : rows) {
-                System.out.print(value + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
